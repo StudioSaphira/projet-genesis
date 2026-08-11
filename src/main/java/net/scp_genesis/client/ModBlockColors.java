4ebbc88ea89d2947ad03073d20e56fc63020cf44
Saphira <studio.saphira.developpement@gmail.com>
2026-08-11 19:15:21 +0200

    Added ModBlockColors

---
+++ src/main/java/net/scp_genesis/client/ModBlockColors.java
@@ -0,0 +1,19 @@
+package net.scp_genesis.client;
+
+import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
+
+import net.scp_genesis.copycatblocks.client.CopycatBlockColor;
+import net.scp_genesis.registry.ModBlocks;
+
+public final class ModBlockColors {
+
+    private ModBlockColors() {}
+
+    public static void register(RegisterColorHandlersEvent.Block event) {
+
+        event.register(
+                new CopycatBlockColor(),
+                ModBlocks.COPYCAT_CUBE.get()
+        );
+    }
+}
