2063523aac915f36269561825b650974d5e1bc7b
Saphira <studio.saphira.developpement@gmail.com>
2026-08-20 22:51:03 +0200

    Registries

--- src/main/java/net/scp_genesis/registry/ModItems.java
+++ src/main/java/net/scp_genesis/registry/ModItems.java
@@ -133,6 +133,7 @@ public class ModItems {
 	public static final DeferredItem<Item> COPYCAT_SLAB = block(ModBlocks.COPYCAT_SLAB);
 	public static final DeferredItem<Item> COPYCAT_WRENCH = REGISTRY.register("copycat_wrench", CopycatWrenchItem::new);
 	public static final DeferredItem<Item> COPYCAT_SCRAPER = REGISTRY.register("copycat_scraper", CopycatScraperItem::new);
+	public static final DeferredItem<Item> COPYCAT_REMOVER = REGISTRY.register("copycat_remover", CopycatRemoverItem::new);
 
 	// Start of user code block custom items
 	// End of user code block custom items
--- src/main/java/net/scp_genesis/registry/ModTabs.java
+++ src/main/java/net/scp_genesis/registry/ModTabs.java
@@ -118,6 +118,7 @@ public class ModTabs {
 			() -> CreativeModeTab.builder().title(Component.translatable("item_group.scp_genesis.copycat_blocks")).icon(() -> new ItemStack(ModBlocks.COPYCAT_CUBE.get())).displayItems((parameters, tabData) -> {
 				tabData.accept(ModItems.COPYCAT_WRENCH.get());
 				tabData.accept(ModItems.COPYCAT_SCRAPER.get());
+				tabData.accept(ModItems.COPYCAT_REMOVER.get());
 				tabData.accept(ModItems.COPYCAT_CUBE.get());
 				tabData.accept(ModItems.COPYCAT_SLAB.get());
 				// tabData.accept(ModItems.COPYCAT_STAIRS.get());
