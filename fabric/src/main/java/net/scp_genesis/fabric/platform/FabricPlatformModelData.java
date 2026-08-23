3368a111a320cfe88de83f479ae12c6ac902c1da
Saphira <studio.saphira.developpement@gmail.com>
2026-08-23 22:08:57 +0200

    FabricPlatform modification

--- fabric/src/main/java/net/scp_genesis/fabric/platform/FabricPlatformModelData.java
+++ fabric/src/main/java/net/scp_genesis/fabric/platform/FabricPlatformModelData.java
@@ -1,4 +1,40 @@
 package net.scp_genesis.fabric.platform;
 
-public class FabricPlatformModelData {
+import net.minecraft.world.level.block.state.BlockState;
+import net.scp_genesis.common.copycatblocks.data.CopycatPart;
+import net.scp_genesis.common.platform.PlatformModelData;
+
+import java.util.EnumMap;
+
+public final class FabricPlatformModelData
+        implements PlatformModelData<FabricPlatformModelData.Data> {
+
+    @Override
+    public Data create(
+            EnumMap<CopycatPart, BlockState> copiedStates
+    ) {
+        return new Data(copiedStates);
+    }
+
+    public static Data empty() {
+        return new Data(
+                new EnumMap<>(CopycatPart.class)
+        );
+    }
+
+    public static final class Data
+            implements PlatformModelData.ModelData {
+
+        private final EnumMap<CopycatPart, BlockState> copiedStates;
+
+        private Data(
+                EnumMap<CopycatPart, BlockState> copiedStates
+        ) {
+            this.copiedStates = copiedStates;
+        }
+
+        public EnumMap<CopycatPart, BlockState> getCopiedStates() {
+            return copiedStates;
+        }
+    }
 }
--- fabric/src/main/java/net/scp_genesis/fabric/platform/FabricPlatformServices.java
+++ fabric/src/main/java/net/scp_genesis/fabric/platform/FabricPlatformServices.java
@@ -1,4 +1,44 @@
 package net.scp_genesis.fabric.platform;
 
-public class FabricPlatformServices {
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.scp_genesis.common.copycatblocks.blockentity.CopycatBlockEntity;
+import net.scp_genesis.common.platform.PlatformModelData;
+import net.scp_genesis.common.platform.PlatformServicesImpl;
+
+public final class FabricPlatformServices
+        implements PlatformServicesImpl {
+
+    private static final PlatformModelData<FabricPlatformModelData.Data>
+            MODEL_DATA =
+            new FabricPlatformModelData();
+
+    @Override
+    public PlatformModelData<?> modelData() {
+        return MODEL_DATA;
+    }
+
+    @Override
+    public void requestModelDataUpdate(
+            BlockEntity blockEntity
+    ) {
+        /*
+         * Fabric does not have NeoForge's ModelDataManager.
+         *
+         * Model data will be consumed directly by the
+         * Fabric Copycat renderer.
+         */
+    }
+
+    @Override
+    public PlatformModelData.ModelData getModelData(
+            BlockEntity blockEntity
+    ) {
+        if (blockEntity instanceof CopycatBlockEntity copycat) {
+            return MODEL_DATA.create(
+                    copycat.getCopiedStates()
+            );
+        }
+
+        return FabricPlatformModelData.empty();
+    }
 }
