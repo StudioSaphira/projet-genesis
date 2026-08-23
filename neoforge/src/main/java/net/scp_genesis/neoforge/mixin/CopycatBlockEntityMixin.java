eb0c092354e29a868c790ae5323fc27538272bd8
Saphira <studio.saphira.developpement@gmail.com>
2026-08-23 18:04:03 +0200

    NeoForge is working now !

--- neoforge/src/main/java/net/scp_genesis/neoforge/mixin/CopycatBlockEntityMixin.java
+++ neoforge/src/main/java/net/scp_genesis/neoforge/mixin/CopycatBlockEntityMixin.java
@@ -1,6 +1,30 @@
 package net.scp_genesis.neoforge.mixin;
 
-//TODO - DELETE THIS FILE
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.client.model.data.ModelData;
+import net.scp_genesis.common.copycatblocks.blockentity.CopycatBlockEntity;
+import net.scp_genesis.common.copycatblocks.data.CopycatPart;
+import net.scp_genesis.neoforge.platform.NeoForgePlatformModelData;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.EnumMap;
+
+@SuppressWarnings("unused")
+@Mixin(CopycatBlockEntity.class)
 public abstract class CopycatBlockEntityMixin {
 
+    @Shadow
+    public abstract EnumMap<CopycatPart, BlockState> getCopiedStates();
+
+    /**
+     * Provides the ModelData used by NeoForge's model system.
+     */
+    public ModelData getModelData() {
+        return new NeoForgePlatformModelData()
+                .create(
+                        getCopiedStates()
+                )
+                .get();
+    }
 }
