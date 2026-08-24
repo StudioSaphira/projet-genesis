07a8021f12a3dca7cfa6bb0c6b19ffe9c852349f
Saphira <studio.saphira.developpement@gmail.com>
2026-08-25 00:19:56 +0200

    Fix

--- common/src/main/java/net/scp_genesis/common/copycatblocks/block/custom/CopycatSlopeBlock.java
+++ common/src/main/java/net/scp_genesis/common/copycatblocks/block/custom/CopycatSlopeBlock.java
@@ -33,6 +33,8 @@ public class CopycatSlopeBlock extends AbstractCopycatBlock {
     public static final EnumProperty<Half> HALF =
             BlockStateProperties.HALF;
 
+
+
     public CopycatSlopeBlock(
             BlockBehaviour.Properties properties
     ) {
